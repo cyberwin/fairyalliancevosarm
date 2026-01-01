b0d76736660e13a43744faa6dac057e704f3afa8
未来之窗软件服务中心 <ymmygrh@126.com>
2026-01-02 05:13:40 +0800

    Create cef.h

---
+++ include/cef/cef.h
@@ -0,0 +1,11 @@
+#ifndef CEF_H_
+#define CEF_H_
+
+#include "cef_base.h"
+#include "internal/cef_config.h"
+#include "internal/cef_types.h"
+#include "internal/cef_app_types.h"
+#include "internal/cef_browser_types.h"
+#include "internal/cef_v8_types.h"
+
+#endif  // CEF_H_
